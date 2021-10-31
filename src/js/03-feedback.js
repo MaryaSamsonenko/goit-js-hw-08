31239cdbfdd765ae0358bbc51e3acb752e63268d
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-10-31 23:59:13 +0200

    еще один вариантБ как убрать проблему с undefined

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,45 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', throttle(onInputTextarea, 1000));
+refs.textarea.addEventListener('input', throttle(onInputTextarea, 1000));
+
+const STORAGE_INPUTS_KEY = 'feedback-form-state';
+const formData = { message: '', email: '' };
+
+populateTextarea();
+
+function onInputTextarea(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_INPUTS_KEY, JSON.stringify(formData));
+  console.log(formData);
+}
+
+function populateTextarea() {
+  const saveMessage = JSON.parse(localStorage.getItem(STORAGE_INPUTS_KEY));
+  if (saveMessage) {
+    // console.log(saveMessage);
+
+    refs.input.value = saveMessage.email;
+    refs.textarea.value = saveMessage.message;
+  }
+}
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_INPUTS_KEY);
+}
+
+//  =========== Option 2 ============
+//  переделала с помощью forEach, чтобы не появлялся undefined при перезагрузке с пустым инпутом
+
 // import throttle from 'lodash.throttle';
 
 // const refs = {
@@ -25,10 +67,10 @@
 // function populateTextarea() {
 //   const saveMessage = JSON.parse(localStorage.getItem(STORAGE_INPUTS_KEY));
 //   if (saveMessage) {
-//     // console.log(saveMessage);
-
-//     refs.input.value = saveMessage.email;
-//     refs.textarea.value = saveMessage.message;
+//     Object.entries(saveMessage).forEach(([name, value]) => {
+//       formData[name] = value;
+//       refs.form.elements[name].value = value;
+//     });
 //   }
 // }
 // function onFormSubmit(event) {
@@ -36,45 +78,3 @@
 //   event.currentTarget.reset();
 //   localStorage.removeItem(STORAGE_INPUTS_KEY);
 // }
-
-//  =========== Option 2 ============
-//  переделала с помощью forEach, чтобы не появлялся undefined при перезагрузке с пустым инпутом
-
-import throttle from 'lodash.throttle';
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  input: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.input.addEventListener('input', throttle(onInputTextarea, 1000));
-refs.textarea.addEventListener('input', throttle(onInputTextarea, 1000));
-
-const STORAGE_INPUTS_KEY = 'feedback-form-state';
-const formData = {};
-
-populateTextarea();
-
-function onInputTextarea(event) {
-  formData[event.target.name] = event.target.value;
-
-  localStorage.setItem(STORAGE_INPUTS_KEY, JSON.stringify(formData));
-  console.log(formData);
-}
-
-function populateTextarea() {
-  const saveMessage = JSON.parse(localStorage.getItem(STORAGE_INPUTS_KEY));
-  if (saveMessage) {
-    Object.entries(saveMessage).forEach(([name, value]) => {
-      formData[name] = value;
-      refs.form.elements[name].value = value;
-    });
-  }
-}
-function onFormSubmit(event) {
-  event.preventDefault();
-  event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_INPUTS_KEY);
-}
